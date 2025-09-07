751d2f40b0ce49baec3330857c960e7cd179d2aa
Random-Baka <waynethomson5@gmail.com>
2025-09-07 15:35:54 +0100

    init

---
+++ Server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.get('/video', (req, res) => {
+    res.sendFile('assets/12/12.mp4', {root: __dirname});
+});
+
+app.listen(4000, () => {
+    console.log('Listending on P400');
+});
